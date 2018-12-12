287589514df9bbff3fd0cfc027e87453c6db1f3a
Andrey Fadeev <me@ciiol.net>
2018-12-12 14:47:46 +0300

    Use more obvious metrics gathering

--- src/hay_cgroup_handler.erl
+++ src/hay_cgroup_handler.erl
@@ -44,31 +44,31 @@ gather_metrics(_State) ->
 %% Internals
 
 gather_cg_memory() ->
-    Keys = [
-        limit,
-        swlimit,
-        usage,
-        swusage,
-        cache,
-        rss,
-        rss_huge,
-        mapped_file,
-        pgpgin,
-        pgpgout,
-        swap,
-        writeback,
-        inactive_anon,
-        active_anon,
-        inactive_file,
-        active_file
+    Metrics = [
+        {limit, fun cg_mem_sup:limit/0},
+        {swlimit, fun cg_mem_sup:swlimit/0},
+        {usage, fun cg_mem_sup:usage/0},
+        {swusage, fun cg_mem_sup:swusage/0},
+        {cache, fun cg_mem_sup:cache/0},
+        {rss, fun cg_mem_sup:rss/0},
+        {rss_huge, fun cg_mem_sup:rss_huge/0},
+        {mapped_file, fun cg_mem_sup:mapped_file/0},
+        {pgpgin, fun cg_mem_sup:pgpgin/0},
+        {pgpgout, fun cg_mem_sup:pgpgout/0},
+        {swap, fun cg_mem_sup:swap/0},
+        {writeback, fun cg_mem_sup:writeback/0},
+        {inactive_anon, fun cg_mem_sup:inactive_anon/0},
+        {active_anon, fun cg_mem_sup:active_anon/0},
+        {inactive_file, fun cg_mem_sup:inactive_file/0},
+        {active_file, fun cg_mem_sup:active_file/0}
     ],
-    gather_metrics(cg_mem_sup, Keys, [<<"cgroup">>, <<"memory">>]).
+    gather_metrics(Metrics, [<<"cgroup">>, <<"memory">>]).
 
 gather_cg_cpu() ->
-    gather_metrics(cg_cpu_sup, [usage], [<<"cgroup">>, <<"cpu">>]).
+    gather_metrics([{usage, fun cg_cpu_sup:usage/0}], [<<"cgroup">>, <<"cpu">>]).
 
-gather_metrics(Module, MetricKeys, KeyPrefix) ->
-    Data = [{Key, erlang:apply(Module, Key, [])} || Key <- MetricKeys],
+gather_metrics(Metrics, KeyPrefix) ->
+    Data = [{Key, Fun()} || {Key, Fun} <- Metrics],
     [
         hay_metrics:construct(gauge, [KeyPrefix, Key], Value)
         || {Key, Value} <- Data, Value =/= undefined
