a835272510168ebf19e541ffb98c4b923d7c93f1
Igor Toporkov <i.toporkov@rbkmoney.com>
2020-10-13 12:35:16 +0300

    Fix formatting in collector

--- src/hay_prometheus_collector.erl
+++ src/hay_prometheus_collector.erl
@@ -16,17 +16,17 @@
 %% called to collect Metric Families
 -spec collect_mf(_, _) -> _.
 collect_mf(Registry, Callback) ->
-  case registry() of
-    Registry -> 
-      Metrics = metrics(),
-      [add_metric_family(Metric, Callback) || Metric <- Metrics],
-      ok;
+    case registry() of
+        Registry -> 
+        Metrics = metrics(),
+        [add_metric_family(Metric, Callback) || Metric <- Metrics],
+        ok;
     _ ->
-      ok
+        ok
   end.
 
 add_metric_family(#metric{type = Type, key = Key, value = Value}, Callback) ->
-  Callback(create_mf(Key, Key, Type, Value)).
+    Callback(create_mf(Key, Key, Type, Value)).
 
 %% called when collector deregistered
 -spec deregister_cleanup(_) -> ok.
@@ -37,7 +37,7 @@ metrics() ->
     hay_vm_handler:gather_metrics(ok) ++ hay_cgroup_handler:gather_metrics(ok). % TODO: configuration?
 
 registry() ->
-  case application:get_env(how_are_you, registry) of
-    {ok, Registy} -> Registy;
-    _ -> default
-  end.
+    case application:get_env(how_are_you, registry) of
+        {ok, Registy} -> Registy;
+        _ -> default
+    end.
